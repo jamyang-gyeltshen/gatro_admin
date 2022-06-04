2b359d7a1775e31b2b74763aa4b1cfc8d05a87b8
Kalu Ram Ghalley@50 <12200050.gcit@rub.edu.bt>
2022-06-04 22:25:09 +0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, Routes, BrowserRouter} from "react-router-dom";
+import './App.css';
+import AddEdit from "./pages/AddEdit";
+import Dashboard from "./pages/Dashboard";
+import View from "./pages/View";
+import Layout from "./pages/layout";
+import About from "./pages/About";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+
+
+export default function App() {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    root.render(
+      <BrowserRouter>
+      <div>
+      <Header position="top-center" />
+      <ToastContainer position="top-center"/>
+      <Routes position="center"> 
+       <Route path="/" element={<Layout />}>
+          <Route exact index element={<Dashboard />} />
+          <Route path="/add" element={<AddEdit />} />
+          <Route path="/update/:id" element={<AddEdit />} />
+          <Route path="/view/:id" element={<View />} />
+          <Route path="/about" element={<About/>}/>
+        </Route>
+      </Routes>
+      </div>
+    </BrowserRouter>
+  
+
+    )
+    
   );
 }
 
-export default App;
+
+ 
